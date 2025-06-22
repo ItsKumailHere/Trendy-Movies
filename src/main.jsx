d22d7589086bc4186564dd290b788d22a07131da
Kumail <kumailn62@gmail.com>
2025-06-22 10:36:29 +0500

    added react-error-boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ErrorBoundary
+      fallback={
+        <div className="bg-red-600">
+          Something went wrong while displaying the app. Please check console to
+          further errors
+        </div>
+      }
+    >
+      <App />
+    </ErrorBoundary>
+  </StrictMode>
+);
