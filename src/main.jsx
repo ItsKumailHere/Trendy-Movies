01a6e3445211f86f5b17b2aa6e105b55cd077bad
Kumail <kumailn62@gmail.com>
2025-07-11 16:14:14 +0500

    added Errror boundary hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import "./index.css";
 import App from "./App.jsx";
 
